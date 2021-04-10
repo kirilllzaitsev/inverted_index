9835dbb4f88fee855008490b2e2f82c0450e977a
kirill.zajcev <kirill.zajcev@murka.com>
2021-04-10 19:05:19 +0300

    feat(server for NLP processing): add ability to convert a set of words to a set of IDs by sending request to a python server

---
+++ text_preprocessing/server.py
@@ -0,0 +1,34 @@
+import asyncio
+from normalizer import Normalizer
+from tokenizer import Tokenizer
+
+
+VOCAB_PATH = '../datasets/aclImdb'
+tokenizer = Tokenizer(vocab_path=VOCAB_PATH)
+normalizer = Normalizer()
+
+
+async def handle_client(reader, writer):
+    request = None
+    while request != 'quit':
+        length_of_message = int.from_bytes(await reader.read(2), byteorder='big')
+        print(f'Received {length_of_message} bytes')
+        request = (await reader.read(1024))
+        request = request.decode('utf8')
+        response = normalizer.normalize(request)
+        response = tokenizer.tokenize(response)
+        response = tokenizer.convert_tokens_to_ids(response)
+        writer.write(len(response).to_bytes(2, byteorder='big'))
+        response = [x.to_bytes(4, byteorder='big') for x in response]
+        for x in response:
+            writer.write(x)
+        await writer.drain()
+    writer.close()
+
+
+async def run_server():
+    server = await asyncio.start_server(handle_client, 'localhost', 11030)
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(run_server())
