c76cd30c690a34d45ddee0442f53e65b5881cdde
kirill.zajcev <kirill.zajcev@murka.com>
2021-04-10 19:20:19 +0300

    feat: add ability to serve clients who search for docs with a word

--- src/com/parallel/computing/Main.java
+++ src/com/parallel/computing/Main.java
@@ -3,15 +3,40 @@ package com.parallel.computing;
 import javafx.util.Pair;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Semaphore;
 
 
 public class Main {
 
+    public static final int PORT = 3333;
+    public static LinkedList<SearchServer> serverList = new LinkedList<>();
+    public static Semaphore numAvailableConnections = new Semaphore(2);
+
     public static void main(String[] args) throws IOException {
         ConcurrentHashMap<Integer, ArrayList<String>> wordToDoc = new ConcurrentHashMap<>();
+
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("Server Started");
+            while (true) {
+                Main.numAvailableConnections.acquire();
+                Socket socket = server.accept();
+                try {
+                    serverList.add(new SearchServer(socket, wordToDoc));
+                } finally {
+                    System.out.println("Available connection slots: " +
+                            Main.numAvailableConnections.availablePermits());
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         String f11 = "datasets/aclImdb/test/neg/0_2.txt";
         String f12 = "datasets/aclImdb/test/neg/1_3.txt";
         String f21 = "datasets/aclImdb/test/neg/2_3.txt";
