e3635a23b92e8c21e125102b971d2ebc2b004646
kirill.zajcev <kirill.zajcev@murka.com>
2021-04-10 19:14:52 +0300

    feat: add printing of final wordToDoc map when all IndexerTasks are done

--- src/com/parallel/computing/Main.java
+++ src/com/parallel/computing/Main.java
@@ -12,13 +12,31 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         ConcurrentHashMap<Integer, ArrayList<String>> wordToDoc = new ConcurrentHashMap<>();
-        String f1 = "datasets/aclImdb/test/neg/0_2.txt";
-        String f2 = "datasets/aclImdb/test/neg/1_3.txt";
-        IndexerTask indexerTask = new IndexerTask(new ArrayList<>(Arrays.asList(
-                new Pair<>(f1, 0),
-                new Pair<>(f2, 1)
+        String f11 = "datasets/aclImdb/test/neg/0_2.txt";
+        String f12 = "datasets/aclImdb/test/neg/1_3.txt";
+        String f21 = "datasets/aclImdb/test/neg/2_3.txt";
+        String f22 = "datasets/aclImdb/test/neg/3_4.txt";
+        IndexerTask indexerTask1 = new IndexerTask(new ArrayList<>(Arrays.asList(
+                new Pair<>(f11, 0),
+                new Pair<>(f12, 1)
         )));
-        Indexer i1 = new Indexer(wordToDoc, indexerTask);
+        IndexerTask indexerTask2 = new IndexerTask(new ArrayList<>(Arrays.asList(
+                new Pair<>(f21, 0),
+                new Pair<>(f22, 1)
+        )));
+        Indexer i1 = new Indexer(wordToDoc, indexerTask1);
+        Indexer i2 = new Indexer(wordToDoc, indexerTask2);
         i1.run();
+        i2.run();
+
+        for(int wordId: wordToDoc.keySet()){
+            System.out.println(wordId);
+            for (String docName:
+                    wordToDoc.get(wordId)) {
+                System.out.print(docName+'\t');
+            }
+            System.out.println();
+        }
+
     }
 }
