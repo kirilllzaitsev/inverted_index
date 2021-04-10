a8239a13986ce4c80fd215ba6c868bde02300386
kirill.zajcev <kirill.zajcev@murka.com>
2021-04-10 19:41:39 +0300

    feat: add read functionality to SearchServer

--- src/com/parallel/computing/SearchServer.java
+++ src/com/parallel/computing/SearchServer.java
@@ -1,10 +1,13 @@
 package com.parallel.computing;
 
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class SearchServer implements Runnable{
+
+public class SearchServer  extends Thread{
     private final Socket socket;
     private final ConcurrentHashMap<Integer, ArrayList<String>> wordToDoc;
 
@@ -15,6 +18,25 @@ public class SearchServer implements Runnable{
 
     @Override
     public void run() {
+
+        try {
+            while (true) {
+                DataInputStream dIn = new DataInputStream(socket.getInputStream());
+                String word = dIn.readUTF();
+                ArrayList<String> docs = this.wordToDoc.getOrDefault(word, new ArrayList<>());
+                this.send(docs);
+            }
+        } catch (NullPointerException ignored) {}
+        catch (IOException e) {
+            this.downService();
+        }
+    }
+
+    private void send(ArrayList<String> docsList) {
+
+    }
+
+    private void downService() {
 
     }
 
