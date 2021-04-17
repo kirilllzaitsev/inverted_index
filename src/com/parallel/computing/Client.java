e26a79a5fad3ffd6a21e94d59b352434fe61eb56
kirill.zajcev <kirill.zajcev@murka.com>
2021-04-17 15:48:57 +0300

    feat(client constructor): added Client constructor

--- src/com/parallel/computing/Client.java
+++ src/com/parallel/computing/Client.java
@@ -2,6 +2,7 @@ package com.parallel.computing;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 
@@ -11,6 +12,12 @@ public class Client {
     BufferedReader in;
 
     public Client(String addr, int port) {
+        try {
+            this.socket = new Socket(addr, port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            System.err.println("Socket failed");
+        }
     }
 
     public void send() throws IOException {
