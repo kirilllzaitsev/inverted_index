9a2f6ef421cc1ca42b07815a634f4bfd61004018
kirill.zajcev <kirill.zajcev@murka.com>
2021-04-17 15:45:50 +0300

    feat(vocab): added wordToId attribute to SearchServer

--- src/com/parallel/computing/SearchServer.java
+++ src/com/parallel/computing/SearchServer.java
@@ -2,19 +2,38 @@ package com.parallel.computing;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 
 
 public class SearchServer  extends Thread{
     private final Socket socket;
     private final ConcurrentHashMap<Integer, ArrayList<String>> wordToDoc;
+    private final HashMap<String, Integer> wordToId = new HashMap<>();
 
     public SearchServer(Socket socket, ConcurrentHashMap<Integer, ArrayList<String>> wordToDoc) {
         this.socket = socket;
         this.wordToDoc = wordToDoc;
+        initWordToIdMap();
+        start();
+    }
+
+    private void initWordToIdMap() {
+        try {
+            Scanner input = new Scanner(new File("datasets/aclImdb/imdb.vocab"));
+            int count = 0;
+            while (input.hasNext()) {
+                this.wordToId.put(input.next(), count);
+                count = count + 1;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -24,7 +43,9 @@ public class SearchServer  extends Thread{
             while (true) {
                 DataInputStream dIn = new DataInputStream(socket.getInputStream());
                 String word = dIn.readUTF();
-                ArrayList<String> docs = this.wordToDoc.getOrDefault(word, new ArrayList<>());
+                ArrayList<String> docs = this.wordToDoc.getOrDefault(
+                        wordToId.getOrDefault(word, -1),
+                        new ArrayList<>());
                 this.send(docs);
             }
         } catch (NullPointerException ignored) {}
