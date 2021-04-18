1d9c2f24a93f57de8739a44a6d6586978b3b9a29
kirill.zajcev <kirill.zajcev@murka.com>
2021-04-18 21:45:49 +0300

    feat(kafka consumer init): add initConsumer to IndexingController

--- src/com/parallel/computing/IndexingController.java
+++ src/com/parallel/computing/IndexingController.java
@@ -1,18 +1,36 @@
 package com.parallel.computing;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 
 
 public class IndexingController extends Thread{
 
     private final ConcurrentHashMap<Integer, ArrayList<String>> wordToDoc;
+    private KafkaConsumer<String, String> consumer;
 
     IndexingController(ConcurrentHashMap<Integer, ArrayList<String>> wordToDoc) {
         this.wordToDoc = wordToDoc;
     }
 
     @Override
-    public void run() {
+    public void run() {}
+
+    private void initConsumer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "index_app");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer.encoding", "UTF-8");
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+
+        consumer = new KafkaConsumer<>(props);
     }
 }
