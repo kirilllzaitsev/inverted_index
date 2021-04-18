ac46cfc20cf9cc99ef12b4547108709f06c39b67
kirill.zajcev <kirill.zajcev@murka.com>
2021-04-18 23:41:25 +0300

    refactoring: split tokenization server module into multiple functions

--- text_preprocessing/server.py
+++ text_preprocessing/server.py
@@ -12,26 +12,37 @@ normalizer = Normalizer()
 LOGGER = logging.get_logger(__name__)
 
 
+def convert_request_to_response(request: str) -> str:
+    response = normalizer.normalize(request)
+    response = tokenizer.tokenize(response)
+    response = tokenizer.convert_tokens_to_ids(response)
+    return response
+
+
+def send_response(writer, response: str) -> None:
+    writer.write(len(response).to_bytes(2, byteorder='big'))
+    LOGGER.info(f'Send from tokenizer: {response}')
+    response = [x.to_bytes(4, byteorder='big') for x in response]
+    for x in response:
+        writer.write(x)
+
+
 async def handle_client(reader, writer):
     request = None
     while request != STOP_PHRASE:
         length_of_message = int.from_bytes(await reader.read(2), byteorder='big')
         LOGGER.info(f'Received {length_of_message} bytes')
         request = (await reader.read(length_of_message))
+
         try:
             request = request.decode('utf-8')
         except UnicodeDecodeError:
             print("Emojis in tweets are not yet supported")
             writer.write(len([]).to_bytes(2, byteorder='big'))
             continue
-        response = normalizer.normalize(request)
-        response = tokenizer.tokenize(response)
-        response = tokenizer.convert_tokens_to_ids(response)
-        writer.write(len(response).to_bytes(2, byteorder='big'))
-        LOGGER.info(f'Send from tokenizer: {response}')
-        response = [x.to_bytes(4, byteorder='big') for x in response]
-        for x in response:
-            writer.write(x)
+
+        response = convert_request_to_response(request)
+        send_response(writer, response)
         await writer.drain()
     writer.close()
 
