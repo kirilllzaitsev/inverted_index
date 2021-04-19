4992aece734053a9fe21e2f4375dbfaba0ce5e7c
kirill.zajcev <kirill.zajcev@murka.com>
2021-04-19 14:27:46 +0300

    feat: add asynchronous requests to tokenization server

--- src/com/parallel/computing/Indexer.java
+++ src/com/parallel/computing/Indexer.java
@@ -4,11 +4,14 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 
 public class Indexer extends Thread{
@@ -17,7 +20,7 @@ public class Indexer extends Thread{
     private final Socket tokenizerSocket = new Socket("localhost", 11030);
     private final DataOutputStream tokenizerOut = new DataOutputStream(tokenizerSocket.getOutputStream());
     private final DataInputStream tokenizerIn = new DataInputStream(tokenizerSocket.getInputStream());
-    private final Vocab vocab = new Vocab();
+    private final Logger logger = Logger.getLogger(Indexer.class.getName());
 
     Indexer(ConcurrentHashMap<Integer, ArrayList<String>> wordToDoc, IndexerTask task) throws IOException {
         this.map = wordToDoc;
@@ -26,35 +29,48 @@ public class Indexer extends Thread{
 
     @Override
     public void run() {
-        for(FileItem x: files2Id){
-            String fileName = x.name;
+        this.files2Id.stream()
+                .map((fi) ->
+                        CompletableFuture.supplyAsync(() -> this.processFileItem(fi))
+                                .exceptionally(this::getFaultyFileItem))
+                .forEach(CompletableFuture::join);
+        this.logger.info("Processed " + this.files2Id.size() + " tweets");
+    }
 
-            String fileContents;
-            try {
-                fileContents = Files.readString(Paths.get(fileName));
-                tokenizerOut.writeUTF(fileContents);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return;
-            }
-            int[] tokenizedText;
-            try {
-                int numTokens = tokenizerIn.readUnsignedShort();
-                if (numTokens == 0){
-                    System.out.println("Failed to tokenize text. Encoding's incompatible");
-                    continue;
-                }
-                tokenizedText = new int[numTokens];
-                for (int i = 0; i < numTokens; ++i) {
-                    tokenizedText[i] = tokenizerIn.readInt();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                return;
-            }
+    private Boolean getFaultyFileItem(Throwable throwable) {
+        this.logger.warning("Something failed when processing file\n" + throwable);
+        return true;
+    }
 
-            updateIndex(tokenizedText, fileName);
+    private boolean processFileItem(FileItem fi) {
+        String fileName = fi.name;
+
+        String fileContents;
+        try {
+            fileContents = new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
+            tokenizerOut.writeUTF(fileContents);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
+        int[] tokenizedText;
+        try {
+            int numTokens = tokenizerIn.readUnsignedShort();
+            if (numTokens == 0){
+                logger.warning("Failed to tokenize text. Encoding's incompatible");
+                return true;
+            }
+            tokenizedText = new int[numTokens];
+            for (int i = 0; i < numTokens; ++i) {
+                tokenizedText[i] = tokenizerIn.readInt();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        updateIndex(tokenizedText, fileName);
+        return true;
     }
 
     private void updateIndex(int[] tokenizedText, String fileName) {
