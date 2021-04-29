81bad628655a002d4dfa42e58187ef3dca5868eb
kirill.zajcev <kirill.zajcev@murka.com>
2021-04-29 16:02:00 +0300

    feat: add sleeping to kafka producer when cluster is not yet initialized

--- kafka/kafka_producer.py
+++ kafka/kafka_producer.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import logging
 from json import dumps
 from time import sleep
 from typing import Optional
@@ -13,6 +14,8 @@ TWITTER_KEY = 'YOUR KEY'
 TWITTER_SECRET = 'YOUR SECRET'
 
 INDEX_APP_TOPIC = 'inverted_index_app'
+
+LOGGER = logging.getLogger(__name__)
 
 
 class StreamListener(tweepy.StreamListener):
@@ -39,6 +42,14 @@ def main():
                         default=['bitcoin', 'war', 'biden'])
     args = parser.parse_args(args)
     tags = args.tags
+    while True:
+        try:
+            producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                                     value_serializer=lambda x: dumps(x).encode('utf-8'))
+            break
+        except ValueError:
+            LOGGER.info("Kafka cluster doesn't exist yet. Waiting 5 sec...")
+            sleep(5)
 
     auth = tweepy.OAuthHandler(TWITTER_APP_KEY, TWITTER_APP_SECRET)
     auth.set_access_token(TWITTER_KEY, TWITTER_SECRET)
