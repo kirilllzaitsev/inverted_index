e9deaa5f77a36457d16a11eba6ddae4816aa6fe5
kzait <kzait@softserveinc.com>
2022-03-17 19:26:30 +0200

    fix: type hints, bug in normalizer

--- text_preprocessing/normalizer.py
+++ text_preprocessing/normalizer.py
@@ -9,13 +9,14 @@ class Normalizer:
     def __init__(self):
         self.lemmatizer = nltk.stem.WordNetLemmatizer()
 
-    def normalize(self, x: Union[list, str]) -> List[str]:
+    def normalize(self, x: Union[list, str]) -> str:
         """
         Accepts text (possibly tokenized) and makes it suitable for machine processing
         """
         x = self._remove_stop_words(x)
         x = self._denoise(x)
         x = self._lemmatize(x)
+        x = " ".join(x)
         return x
 
     def _remove_stop_words(self, x: Union[list, str]) -> List[str]:
@@ -27,13 +28,13 @@ class Normalizer:
         stop_words = set(nltk.corpus.stopwords.words("english"))
         return [w for w in x if w not in stop_words]
 
-    def _lemmatize(self, x: Union[list, str]) -> str:
+    def _lemmatize(self, x: Union[list, str]) -> List[str]:
         """
         Removes endings,
         """
-        if isinstance(x, list):
-            x = " ".join(x)
-        x = self.lemmatizer.lemmatize(x)
+        if isinstance(x, str):
+            x = nltk.word_tokenize(x)
+        x = [self.lemmatizer.lemmatize(w) for w in x]
         return x
 
     def _denoise(self, x: Union[list, str]) -> str:
