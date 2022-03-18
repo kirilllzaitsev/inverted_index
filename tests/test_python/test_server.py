2b43609f51d5dc63865edb5acb9f9d2dc66dbec9
cyberpunk317 <44346535+cyberpunk317@users.noreply.github.com>
2022-03-18 17:11:03 +0200

    Refactor CI (#13)
    * chore: rename Dockerfile for client
    
    * refactor: update .gitignore with python files
    
    * feat: materialize some dockerfiles
    
    * refactor: upd type hint
    
    * refactor: remove class methods
    
    * BREAKING CHANGE: upd python to 3.8
    
    * tests: add pytest.ini
    
    * tests: add python tests
    
    * fix: change tests location
    
    * refactor: fix type hints, add logging msgs
    
    * fix: type hints, bug in normalizer
    
    * chore: add __init__.py
    
    * fix: add bin dir to .gitignore
    
    * refactor: remove comment
    
    * fix: add missing nltk pkg
    
    * chore: rename build.yml
    
    * feat: add python CI
    
    * fix: set env manager to pipenv
    
    * refactor: minor python upd
    
    * tests: fix normalizer test
    
    * ci: reduce CI paths scope
    
    * ci(python): add caching
    
    * fix: upd Pipfile
    
    * ci: fix flake8
    
    * debug: ci
    
    * fix: ci
    
    Co-authored-by: kzait <kzait@softserveinc.com>

--- tests/test_python/test_server.py
+++ tests/test_python/test_server.py
@@ -1,15 +1,77 @@
-from unittest import TestCase
+import struct
+import asyncio
+
+import pytest
+
+from text_preprocessing.server import (
+    run_server,
+    convert_request_to_response,
+    handle_client,
+    STOP_PHRASE,
+    TOKENIZER_HOST,
+    TOKENIZER_PORT,
+)
+from text_preprocessing.tokenizer import Tokenizer
 
 
-class TestTokenizationServer(TestCase):
-    def test_convert_request_to_response(self):
-        raise NotImplementedError
+@pytest.fixture(scope="module")
+def tokenizer(vocab_path="vocab/imdb.vocab"):
+    tokenizer = Tokenizer(vocab_path)
+    return tokenizer
 
-    def test_send_response(self):
-        raise NotImplementedError
 
-    def test_handle_client(self):
-        raise NotImplementedError
+@pytest.fixture(scope="module")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
 
-    def test_run_server(self):
-        raise NotImplementedError
+
+@pytest.fixture(scope="module")
+def server(event_loop, callable=handle_client, unused_tcp_port=TOKENIZER_PORT):
+    cancel_handle = asyncio.ensure_future(
+        run_server(callback=callable, port=unused_tcp_port), loop=event_loop
+    )
+    event_loop.run_until_complete(asyncio.sleep(0.01))
+
+    try:
+        yield unused_tcp_port
+    finally:
+        cancel_handle.cancel()
+
+
+class TestTokenizationServer:
+    def test_convert_request_to_response(self, tokenizer):
+        message = "memory ball"
+
+        res_tokens = convert_request_to_response(message)
+        exp_tokens = tokenizer.convert_tokens_to_ids(message.split(" "))
+
+        assert res_tokens == exp_tokens
+
+    @pytest.mark.asyncio
+    async def test_run_server(self, server, tokenizer):
+        message = "memory ball"
+        reader, writer = await asyncio.open_connection(TOKENIZER_HOST, server)
+
+        def send_msg(text):
+
+            msg_len = len(text.encode())
+            writer.write(struct.pack(">I", msg_len))
+            writer.write(text.encode())
+
+        send_msg(message)
+
+        resp_len = int.from_bytes(await reader.read(2), byteorder="big")
+        res_tokens = [
+            int.from_bytes(await reader.read(4), byteorder="big")
+            for _ in range(resp_len)
+        ]
+        send_msg(STOP_PHRASE)
+        await writer.drain()
+        writer.close()
+        await writer.wait_closed()
+
+        exp_tokens = tokenizer.convert_tokens_to_ids(message.split(" "))
+
+        assert res_tokens == exp_tokens
